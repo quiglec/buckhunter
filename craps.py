efabd8977685a20ec73b2324a511bbdd8df26b35
Bryan Quigley <bquigley@tulane.edu>
2019-01-01 22:55:51 -0600

    fixed spacing

--- craps.py
+++ craps.py
@@ -1,5 +1,5 @@
 """
-Debugging program for buckhnter app. Run in terminal before running the real app so you can play some craps.
+Debugging program for buckhunter app. Run in terminal before running the real app so you can play some craps.
 """
 
 import random
@@ -9,7 +9,6 @@ random.seed()
 
 def diceroll():
 	input("Press Enter to roll dice")
-
 	dice_1 = random.randint(1, 6)
 	dice_2 = random.randint(1, 6)
 	print('Dice roll is a ' + str(dice_1) + ' and a ' + str(dice_2) + '\n')
@@ -17,41 +16,29 @@ def diceroll():
 
 def dice_reader():
 	x, y = diceroll()
-
 	if x + y in [2, 3, 12]:
 		return(0)
-
 	elif x + y in [7, 11]:
 		return(1)
-
 	else:
 		return(x + y)
 
-
 def game_start():
-
 	point = 0
-
 	while point == 0:
 		result = dice_reader()
-
 		if result in [4, 5, 6, 8, 9, 10]:
 			print('Point established as ' + str(result) + '\n')
 			point = result
 			break
-
 		if result == 0:
 			print('CRAPS!')
 			break
-
 		if result == 1:
 			print('Pass Line Wins!')
 			break
 	
-
 	if point != 0:
-
-
 		while point != 0:
 			x, y = diceroll()
 			if x + y == 7:
@@ -60,8 +47,5 @@ def game_start():
 			if x + y == point:
 				print('Pass Line Wins!')
 				break	
-		
-
-
 
 game_start()
